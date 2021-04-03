dc4514864cd227dfd48c70619a0069fa0bc93165
ArindamBiswas-dev <arindambiswas616@gmail.com>
2021-04-03 19:04:56 +0530

    Adding new card design for Projects section

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,65 +6,64 @@ import style from '../styles/homePage.module.css';
 function ProjectCard({ title, imageUrl, githubLink, des, liveUrl }) {
   return (
     <Flex
-      w={{ base: '100%', sm: '100%', md: '300px', lg: '450px' }}
-      h="500px"
-      flexDirection="column"
-      justifyContent="space-between"
-      alignItems="center"
-      p="4"
-      boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px;"
+      w={{ base: '100%', sm: '100%', md: '300px', lg: '350px' }}
+      h="450px"
+      boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px"
       my="5"
-      rounded="md"
+      borderRadius="10px"
+      flexDir="column"
     >
-      <Heading size="md" className={style.headingFont}>
-        {title}
-      </Heading>
       <Image
         src={imageUrl}
         alt="project image"
         h="200px"
         w="100%"
         objectFit="cover"
-        border="5px solid #2730B3"
-        position="relative"
+        borderRadius="10px 10px 40px 0px"
       />
-      <Box h="100px">
-        <Text textAlign="justify" className={style.normalFont}>
-          {des}
-        </Text>
+      <Box p="4">
+        <Heading size="md" mb="3" className={style.headingFont}>
+          {title}
+        </Heading>
+        <Box h="150px">
+          <Text textAlign="justify" className={style.normalFont}>
+            {des}
+          </Text>
+        </Box>
+        <Flex
+          justify="space-between"
+          alignItems="center"
+          flexDirection="row"
+          w="100%"
+          mt="3"
+        >
+          <Link
+            href={githubLink}
+            color="#2730B3"
+            isExternal
+            display="flex"
+            alignItems="center"
+            target="_blank"
+          >
+            <Text mr="5px" className={style.normalFont} fontWeight="semibold">
+              Github
+            </Text>
+            <FaGithub />
+          </Link>
+          <Link
+            href={liveUrl}
+            color="#2730B3"
+            isExternal
+            display="flex"
+            alignItems="center"
+          >
+            <Text mr="5px" className={style.normalFont} fontWeight="semibold">
+              Live url
+            </Text>
+            <FaExternalLinkAlt />
+          </Link>
+        </Flex>
       </Box>
-      <Flex
-        justify="space-between"
-        alignItems="center"
-        flexDirection="row"
-        w="100%"
-      >
-        <Link
-          href={githubLink}
-          color="#2730B3"
-          isExternal
-          display="flex"
-          alignItems="baseline"
-          target="_blank"
-        >
-          <Text mr="5px" className={style.normalFont} fontWeight="semibold">
-            Github
-          </Text>
-          <FaGithub />
-        </Link>
-        <Link
-          href={liveUrl}
-          color="#2730B3"
-          isExternal
-          display="flex"
-          alignItems="baseline"
-        >
-          <Text mr="5px" className={style.normalFont} fontWeight="semibold">
-            Live url
-          </Text>
-          <FaExternalLinkAlt />
-        </Link>
-      </Flex>
     </Flex>
   );
 }
